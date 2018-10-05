2a89d71330259cee09ccc67b178864ce85e92905
gajjala-sreekanthreddy <srikanthreddy254@gmail.com>
2018-10-05 16:08:48 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ 5th Oct 2018/Module 4/m4/4.2 Evaluate Postfix/EvaluatePostfix.java
@@ -0,0 +1,14 @@
+
+public class EvaluatePostfix {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<Integer>();
+
+        while (!StdIn.isEmpty()) {
+            String s = StdIn.readString();
+            if      (s.equals("+")) stack.push(stack.pop() + stack.pop());
+            else if (s.equals("*")) stack.push(stack.pop() * stack.pop());
+            else stack.push(Integer.parseInt(s));
+        }
+        StdOut.println(stack.pop());
+    }
+}
